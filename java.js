c407be555e3189bd172d7a442f225aeda27126e2
MadeyraOne <161262391+MadeyraOne@users.noreply.github.com>
2024-03-12 18:29:01 +0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,3 +1,4 @@
+//click to activation burger-menu
 document.querySelector('.burger').addEventListener('click', function() {
    this.classList.toggle('active');
    document.querySelector('.nav').classList.toggle('open');
@@ -5,16 +6,17 @@ document.querySelector('.burger').addEventListener('click', function() {
 
 
 
+//varible*
 const prevButton = document.getElementById('prev-slide');
 const nextButton = document.getElementById('next-slide');
 const imageList = document.querySelector('.image-list');
 
 // Обработчик события для кнопки "назад"
 prevButton.addEventListener('click', () => {
-  imageList.scrollLeft -= 300; // Прокрутка влево на 300px (можно подстроить под ширину изображения)
+  imageList.scrollLeft -= 630;
 });
 
 // Обработчик события для кнопки "вперед"
 nextButton.addEventListener('click', () => {
-  imageList.scrollLeft += 300; // Прокрутка вправо на 300px (можно подстроить под ширину изображения)
+  imageList.scrollLeft += 630;
 });
